a1b4b6f1e7326d54401d8c8e98010628312e60fe
aaron <aaronstepbystep@gmail.com>
2020-04-29 20:33:29 +0800

    2020.04.29 邀请记录

--- app/src/main/java/com/pro/bityard/fragment/my/InviteFragment.java
+++ app/src/main/java/com/pro/bityard/fragment/my/InviteFragment.java
@@ -8,7 +8,7 @@ import com.pro.bityard.base.BaseFragment;
 
 import butterknife.BindView;
 
-public class InviteFragment extends BaseFragment {
+public class InviteFragment extends BaseFragment implements View.OnClickListener {
     @BindView(R.id.text_title)
     TextView text_title;
 
@@ -21,6 +21,8 @@ public class InviteFragment extends BaseFragment {
     protected void initView(View view) {
 
         text_title.setText(R.string.text_affiliate_stats);
+
+        view.findViewById(R.id.img_back).setOnClickListener(this);
     }
 
     @Override
@@ -36,5 +38,14 @@ public class InviteFragment extends BaseFragment {
     @Override
     protected void initData() {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.img_back:
+                getActivity().finish();
+                break;
+        }
     }
 }
