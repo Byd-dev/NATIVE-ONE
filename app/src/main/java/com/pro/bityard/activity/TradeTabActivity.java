10c32da084ce664282c00019c809676eefa78832
aaron <aaronstepbystep@gmail.com>
2021-03-17 14:09:41 +0800

    2021.03.17 完善了行情socket19

--- app/src/main/java/com/pro/bityard/activity/TradeTabActivity.java
+++ app/src/main/java/com/pro/bityard/activity/TradeTabActivity.java
@@ -127,7 +127,6 @@ public class TradeTabActivity extends BaseActivity implements View.OnClickListen
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
             }
             @Override
             public void onPageSelected(int position) {
