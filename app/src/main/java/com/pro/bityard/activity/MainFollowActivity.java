353579db17eb5b8715abb4de523cbdca3c8097a4
switch <wengruigan@outlook.com>
2021-05-05 18:11:44 +0800

    修改了tab2

--- app/src/main/java/com/pro/bityard/activity/MainFollowActivity.java
+++ app/src/main/java/com/pro/bityard/activity/MainFollowActivity.java
@@ -2148,7 +2148,6 @@ public class MainFollowActivity extends BaseActivity implements Observer, View.O
                 layout_circle.setVisibility(View.GONE);
                 layout_my.setVisibility(View.GONE);
                 layout_status.setVisibility(View.VISIBLE);
-
                 radioButton_0.setChecked(false);
                 radioButton_1.setChecked(true);
                 radioButton_2.setChecked(false);
