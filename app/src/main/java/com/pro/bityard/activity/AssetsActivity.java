aab85ca84fce00c928d77bf016bc7d62c72bf836
switch <wengruigan@outlook.com>
2021-05-17 17:30:18 +0800

    修改了资产页面4

--- app/src/main/java/com/pro/bityard/activity/AssetsActivity.java
+++ app/src/main/java/com/pro/bityard/activity/AssetsActivity.java
@@ -56,7 +56,6 @@ public class AssetsActivity extends BaseActivity implements View.OnClickListener
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Util.setTheme(this);
-
     }
 
     @Override
